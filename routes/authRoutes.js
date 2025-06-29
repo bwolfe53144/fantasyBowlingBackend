74f69da5791c6cf975f0069db5b5f637652655d3
bgwolfe <bwolfe53144@yahoo.com>
2025-06-28 21:53:59 -0500

    added /auth to reset password path

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/user", authController.getUser);
 
 router.post("/auth/forgot-password", authController.forgotPassword);
-router.post("/reset-password/:token", authController.resetPassword);
+router.post("/auth/reset-password/:token", authController.resetPassword);
 router.post("/signin", authController.login);
 router.post("/signup", validateUser, authController.signup);
 
