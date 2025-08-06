997952acc2f26dee00eedca9b530c6aed6500080
bgwolfe <bwolfe53144@yahoo.com>
2025-08-05 20:32:43 -0500

    Added password check to reset password

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { validateUser } = require("../controllers/validateController");
+const { validateUser, validatePasswordOnly } = require("../controllers/validateController");
 const authController = require("../controllers/authController");
 const router = express.Router();
 
 router.get("/user", authController.getUser);
 
 router.post("/auth/forgot-password", authController.forgotPassword);
-router.post("/auth/reset-password/:token", authController.resetPassword);
+router.post("/auth/reset-password/:token", validatePasswordOnly, authController.resetPassword);
 router.post("/signin", authController.login);
 router.post("/signup", validateUser, authController.signup);
 
